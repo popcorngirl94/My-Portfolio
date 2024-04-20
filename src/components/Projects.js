7f5637c32096672bf577b6b643ad5ab957509d26
popcorngirl94 <grishmasht17@gmail.com>
2024-04-20 21:37:31 +0545

    2nd

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/pro1.png";
+import projImg2 from "../assets/img/pro2.png";
+import projImg3 from "../assets/img/pro3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -12,18 +12,18 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "DigiLock",
+      description: "a digital locker",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Woodley",
+      description: "Furniture Ecommerce App",
       imgUrl: projImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Thought Tracker",
+      description: "Emotion Tracker App",
       imgUrl: projImg3,
     },
    
@@ -38,7 +38,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>Throughout my academic journey, I have completed various projects, including the development of a Digital Document Locker using the Laravel framework, a Full Stack E-commerce Website using Laravel, and Emotion Tracker - a Sentiment Analysis app using python. </p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
